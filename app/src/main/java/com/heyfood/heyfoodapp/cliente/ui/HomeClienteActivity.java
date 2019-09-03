72ac1a244af280e30514b3a3d5bf9b7224e6ee9b
bruno <bruno.silvaleite15@gmail.com>
2019-09-03 19:42:53 -0300

    Transferência das recomendações para outra activity

--- app/src/main/java/com/heyfood/heyfoodapp/cliente/ui/HomeClienteActivity.java
+++ app/src/main/java/com/heyfood/heyfoodapp/cliente/ui/HomeClienteActivity.java
@@ -46,14 +46,9 @@ public class HomeClienteActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView nome;
-    TextView mensagem;
     TextView nomeMenu;
     TextView emailMenu;
     Cliente cliente;
-    RecyclerView recyclerView;
-    private AvaliacaoRestauranteDAO avaliacaoRestauranteDAO;
-    private Recomendacao recomendacao;
-    private List<Restaurante> restaurantesRecomendados;
     public static Context contexto;
 
     @Override
@@ -61,23 +56,11 @@ public class HomeClienteActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_cliente);
         contexto = this;
-        recomendacao = new Recomendacao();
-        restaurantesRecomendados = recomendacao.getListaRestaurantesRecomendados();
-
         cliente = Sessao.instance.getCliente();
 
         //Bem vindo 'nome do usuario'
         nome = findViewById(R.id.textBoasVindasClienteId);
         nome.setText(String.format("Bem vindo, %s!", cliente.getUsuario().getPessoa().getNome()));
-        mensagem = findViewById(R.id.textRecomendacoes);
-        recyclerView = findViewById(R.id.recyclerViewHome);
-
-        if(restaurantesRecomendados.size()==0){
-            mensagem.setText("Infelizmente ainda não temos sugestões para você, tente avaliar mais restaurantes.");
-        }
-        else{
-            mensagem.setText("Essas são nossas sugestões para você:");
-        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -95,151 +78,6 @@ public class HomeClienteActivity extends AppCompatActivity
         nomeMenu.setText(cliente.getUsuario().getPessoa().getNome());
         emailMenu = headerView.findViewById(R.id.emailMenuId);
         emailMenu.setText(cliente.getUsuario().getPessoa().getContato().getEmail());
-
-        AdapterRestaurante adapter = new AdapterRestaurante(recomendacao.getListaRestaurantesRecomendados());
-        final List<Restaurante> finalListaRestaurante = recomendacao.getListaRestaurantesRecomendados();
-
-        //Configurar Recyclerview
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.addItemDecoration( new DividerItemDecoration(this, LinearLayout.VERTICAL));
-        recyclerView.setAdapter( adapter );
-
-        recyclerView.addOnItemTouchListener(
-                new RecyclerItemClickListener(
-                        getApplicationContext(),
-                        recyclerView,
-                        new RecyclerItemClickListener.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                final Restaurante restaurante = finalListaRestaurante.get(position);
-
-                                avaliacaoRestauranteDAO = new AvaliacaoRestauranteDAO(contexto);
-                                ArrayList<AvaliacaoRestaurante> avaliacoes;
-                                avaliacoes = avaliacaoRestauranteDAO.getAvaliacoes(restaurante.getId());
-
-                                Double soma = 0.0;
-
-                                for(AvaliacaoRestaurante avaliacao: avaliacoes){
-                                    soma = soma + avaliacao.getNota();
-                                }
-                                Double media = soma / avaliacoes.size();
-
-                                StringBuilder mensagem = new StringBuilder();
-                                mensagem.append("Endereço: \n");
-                                mensagem.append(restaurante.getEndereco().getRua() + ", " + restaurante.getEndereco().getNumero() + "\n");
-                                mensagem.append(restaurante.getEndereco().getBairro() + "\n");
-                                mensagem.append(restaurante.getEndereco().getCidade() + "\n\n");
-                                mensagem.append("Fone: " + restaurante.getContato().getTelefone() + "\n");
-                                mensagem.append("Email: " + restaurante.getContato().getEmail() + "\n");
-                                mensagem.append("Média: " + media.toString() + "\n");
-
-                                mensagem.append(restaurante.getContato().getSite());
-
-                                AlertDialog.Builder dialog = new AlertDialog.Builder(HomeClienteActivity.this);
-
-                                //Configura botão
-                                LinearLayout linearLayout = new LinearLayout(contexto);
-                                linearLayout.setOrientation(LinearLayout.VERTICAL);
-                                Button botao = new Button(contexto);
-                                linearLayout.addView(botao);
-                                botao.setText("Listar pratos");
-                                botao.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        Sessao.instance.setRestaurante(restaurante);
-                                        Intent novaTela = new Intent(contexto, ListarPratosActivity.class);
-                                        startActivity(novaTela);
-                                    }
-                                });
-
-                                //Configura titulo e mensagem
-                                dialog.setTitle(restaurante.getNome());
-                                dialog.setMessage(mensagem.toString());
-                                dialog.setView(linearLayout);
-                                dialog.setCancelable(true);
-
-                                if (Sessao.instance.getProprietario() != null){
-                                    dialog.setNegativeButton("EDITAR", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            Sessao.instance.setRestaurante(restaurante);
-                                            Intent novaTela = new Intent(contexto, AtualizarRestauranteActivity.class);
-                                            startActivity(novaTela);
-
-                                        }
-                                    });
-
-                                    //Configura acoes para o botao
-                                    dialog.setPositiveButton("CADASTRAR PRATO", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            Sessao.instance.setRestaurante(restaurante);
-                                            Intent novaTela = new Intent(contexto, CadastrarPratoActivity.class);
-                                            startActivity(novaTela);
-
-                                        }
-                                    });
-                                } else{
-                                    dialog.setPositiveButton("Avaliar", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-
-                                        }
-                                    });
-                                    final RatingBar ratingBar = new RatingBar(contexto);
-                                    ratingBar.setMax(5);
-                                    ratingBar.setRating(0.0f);
-                                    ratingBar.setNumStars(5);
-                                    if (avaliacaoRestauranteDAO.getAvaliacao(Sessao.instance.getCliente().getId(), restaurante.getId()) != null){
-                                        ratingBar.setRating(avaliacaoRestauranteDAO.getAvaliacao(Sessao.instance.getCliente().getId(), restaurante.getId()).getNota());
-                                    }
-                                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams( LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT );
-                                    ratingBar.setLayoutParams(lp);
-                                    linearLayout.addView(ratingBar);
-
-                                    dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            if (avaliacaoRestauranteDAO.getAvaliacao(Sessao.instance.getCliente().getId(), restaurante.getId()) == null) {
-                                                AvaliacaoRestaurante avaliacao = new AvaliacaoRestaurante();
-                                                avaliacao.setRestaurante(restaurante);
-                                                avaliacao.setCliente(Sessao.instance.getCliente());
-                                                avaliacao.setNota(ratingBar.getRating());
-                                                AvaliacaoRestauranteDAO avaliacaoRestauranteDAO = new AvaliacaoRestauranteDAO(contexto);
-                                                avaliacaoRestauranteDAO.cadastrar(avaliacao);
-                                            }
-                                            else {
-                                                AvaliacaoRestaurante avaliacao = new AvaliacaoRestaurante();
-                                                avaliacao = avaliacaoRestauranteDAO.getAvaliacao(Sessao.instance.getCliente().getId(), restaurante.getId());
-                                                avaliacao.setNota(ratingBar.getRating());
-                                                AvaliacaoRestauranteDAO avaliacaoRestauranteDAO = new AvaliacaoRestauranteDAO(contexto);
-                                                avaliacaoRestauranteDAO.updateNota(avaliacao);
-                                            }
-                                        }
-                                    });
-                                }
-
-                                //Criar e exbir o alertDialog
-                                dialog.create();
-                                dialog.show();
-
-                            }
-
-                            @Override
-                            public void onLongItemClick(View view, int position) {
-
-                            }
-
-                            @Override
-                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                            }
-                        }
-                )
-        );
-
 
     }
 
@@ -290,7 +128,11 @@ public class HomeClienteActivity extends AppCompatActivity
             startActivity(novaTela);
 
         } else if (id == R.id.listarRestauranteId) {
-            Intent novaTela = new Intent (this, ListarRestaurantes.class);
+            Intent novaTela = new Intent(this, ListarRestaurantes.class);
+            startActivity(novaTela);
+
+        } else if (id == R.id.recomendacoesId){
+            Intent novaTela = new Intent(this, RecomendacoesActivity.class);
             startActivity(novaTela);
 
         } else if (id == R.id.sairId) {
